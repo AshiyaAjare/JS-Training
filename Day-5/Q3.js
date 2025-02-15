789b1e10e2215328f86793d0ce63e15f73d6cdbf
Ashiya Ajare <ashiya.ajare@joshsoftware.com>
2025-02-15 19:55:28 +0530

    Add Day-5 assignments

---
+++ Day-5/Q3.js
@@ -0,0 +1,24 @@
+/*
+ Share a minimum four differentiation between regular function and arrow functions with one examples
+ */
+
+// Syntax:
+// Regular Function: Uses the 'function' keyword.
+// Arrow Function: Uses the '=>' syntax.
+
+// new keyword:
+// Regular Function: Can be used as a constructor with the new keyword.
+// Arrow Function: Cannot be used as a constructor.
+
+
+// this Binding:
+// Regular Function: Has its own this, determined by how function is called.
+// Arrow Function: Inherits this from the surrounding scope, lexical 'this'.
+
+//Hoisting:
+//Regular Function: Can be hoisted, can be called before function definitions.
+//Arrow Function: Cannot be hoisted, must be defined before they are called.
+
+// Arguments:
+// Regular Function: Has its own arguments object
+// Arrow Function: Does not have its own arguments object, but inherits it from the surrounding scope.
