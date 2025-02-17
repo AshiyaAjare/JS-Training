d448919db4beb936edc4d35e5e11bd8589a623e2
Ashiya Ajare <ashiya.ajare@joshsoftware.com>
2025-02-17 11:06:04 +0530

    Assignment 1 - Basic To-do list in react JS

---
+++ list-app/src/App.tsx
@@ -0,0 +1,50 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [taskInput, setTaskInput] = useState<string>('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskInput(e.target.value);
+  }
+
+  const addTask = () => {
+    const newTask = taskInput;
+    if(newTask !== ''){
+      setTasks([...tasks, newTask]);
+      setTaskInput('');
+    }
+    else{
+      alert('Please enter a task');
+    }
+  };
+
+  const deleteTask = (indexToRemove: number) => {
+    const newTasks = tasks.filter((_, index) => index !== indexToRemove);
+    setTasks(newTasks);
+  }
+
+  return (
+    
+    <div>
+      <h1>Task List</h1>
+      { tasks.map((task, index) => {
+          return <div key={index}>
+            {task}
+            <button onClick={() => {deleteTask(index)}}>Delete</button>
+          </div>
+        })
+      }
+      <input 
+      type="text"
+      value = {taskInput}
+      onChange = {handleInputChange}
+      placeholder = "Enter a task"
+      />
+      <button onClick = {addTask}>Add Task</button>
+    </div>
+  )
+}
+
+export default App;
