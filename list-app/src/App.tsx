0fefc037da88436d031a6b081d86bb09a06b1381
Ashiya Ajare <ashiya.ajare@joshsoftware.com>
2025-02-21 07:55:18 +0530

    Remove use of index as key and use of shorten syntax

--- list-app/src/App.tsx
+++ list-app/src/App.tsx
@@ -1,51 +1,42 @@
-import {useState} from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState<string[]>([]);
-  const [taskInput, setTaskInput] = useState<string>('');
+  const [taskInput, setTaskInput] = useState("");
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setTaskInput(e.target.value);
-  }
 
   const addTask = () => {
-    const newTask = taskInput;
-    if(newTask !== ''){
-      setTasks([...tasks, newTask]);
-      setTaskInput('');
-    }
-    else{
-      alert('Please enter a task');
-    }
+    if (!taskInput.trim()) return alert("Please enter a task");
+    if (tasks.includes(taskInput)) return alert("Task already exists!"); // Prevent duplicates
+    setTasks([...tasks, taskInput]);
+    setTaskInput("");
   };
 
-  const deleteTask = (indexToRemove: number) => {
-    const newTasks = tasks.filter((_, index) => index !== indexToRemove);
-    setTasks(newTasks);
-  }
+  const deleteTask = (taskToRemove: string) =>
+    setTasks(tasks.filter((task) => task !== taskToRemove));
 
   return (
-    
     <div>
       <h1>Task List</h1>
-      { tasks.map((task: string, index: number) => {
-          return <div key={index}>
-            <input type="checkbox" />
-            {task}
-            <button onClick={() => {deleteTask(index)}}>Delete</button>
-          </div>
-        })
-      }
-      <input 
-      type="text"
-      value = {taskInput}
-      onChange = {handleInputChange}
-      placeholder = "Enter a task"
+      {tasks.map((task) => (
+        <div key={task}>
+          <input type="checkbox" />
+          {task}
+          <button onClick={() => deleteTask(task)}>Delete</button>
+        </div>
+      ))}
+      <input
+        type="text"
+        value={taskInput}
+        onChange={handleInputChange}
+        placeholder="Enter a task"
       />
-      <button onClick = {addTask}>Add Task</button>
+      <button onClick={addTask}>Add Task</button>
     </div>
-  )
+  );
 }
 
 export default App;
