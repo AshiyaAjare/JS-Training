e6fd04746470a4988515bd99befc892b14fda201
Ashiya Ajare <ashiya.ajare@joshsoftware.com>
2025-02-21 07:28:03 +0530

    Test functionality for arraySize=25 in Type-conversion/Q4.js

--- Day-1/Type-Conversion/Q4.js
+++ Day-1/Type-Conversion/Q4.js
@@ -16,3 +16,14 @@ while (i <= 10) {
     i++;
 }
 console.log("Loop ended.");
+
+
+//for arraySize=25
+arraySize = 25;
+let arr2 = [];
+let j = 1;
+while (j <= arraySize) {
+arr2.push(j);
+j++;
+}
+console.log(arr2); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]                                       
