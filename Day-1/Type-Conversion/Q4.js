afcf133e255b38176a12c59b9f0d49d9e1448443
Ashiya Ajare <ashiya.ajare@joshsoftware.com>
2025-02-21 07:24:16 +0530

    Add while loop in Type-conversion/Q4.js

--- Day-1/Type-Conversion/Q4.js
+++ Day-1/Type-Conversion/Q4.js
@@ -1,7 +1,18 @@
 //4. Accomplish the same using a while loop. 
 
-for (let i = 1; i <= 10; i++) {
+
+//FOR LOOP
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 5) break;
+//     console.log(i);
+// }
+// console.log("Loop ended.");
+
+//WHILE LOOP
+let i = 1;
+while (i <= 10) {
     if (i === 5) break;
     console.log(i);
+    i++;
 }
 console.log("Loop ended.");
